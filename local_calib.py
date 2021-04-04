1d21395f27f08d189d81d005bfabfbf9e1148198
naeim <naeimrf@gmail.com>
2021-04-04 08:37:17 +0200

    V1.0 Two approaches to generate samples from calibrated parameters

--- local_calib.py
+++ local_calib.py
@@ -3,10 +3,9 @@ import local_builder as lb
 import numpy as np, seaborn as sns, pandas as pd
 import matplotlib.pyplot as plt
 from collections import Counter
-from scipy.stats import pearsonr
-from skopt.space import Space
-from skopt.sampler import Lhs
-from itertools import product
+from scipy import stats, linalg
+import math, random
+from SALib.sample import latin
 
 start_time = time.time()
 test_path = os.path.join(os.getcwd(), lb.CaseName)
@@ -401,7 +400,7 @@ def get_simulation_runs(buildings, simulations):
 
 
 def compare_results(buildings, simulations, measurements,
-                    area, alpha, per_building=False, all_buildings=True):
+                    area, alpha, per_building=False, all_buildings=True, plots=True):
     # prepare simulation results to compare - - -
     total_sim_result = get_simulation_runs(buildings, simulations)
 
@@ -419,10 +418,10 @@ def compare_results(buildings, simulations, measurements,
             err = abs((reference[nbr] / ep_area - value / ep_area) / (reference[nbr] / ep_area)) * 100
             errors[nbr].update({key: err})
 
-        if per_building:
+        if plots and per_building:
             passed_cases_one_building(errors, nbr, alpha)
 
-    if all_buildings:
+    if plots and all_buildings:
         passed_cases_all_buildings(errors, alpha)
 
     # KEEP acceptable buildings with respect to alpha!
@@ -434,11 +433,26 @@ def compare_results(buildings, simulations, measurements,
     return total_sim_result, reference, errors, acceptable
 
 
-def get_correlation(x1, y1, **kws):
-    (r, p) = pearsonr(x1, y1)
+def get_correlation_pearsonr(x1, y1, **kws):
+    (r, p) = stats.pearsonr(x1, y1)
     ax = plt.gca()
-    ax.annotate("r={:.2f}".format(r), xy=(.1, .9), xycoords=ax.transAxes)
-    ax.annotate("p={:.3f}".format(p), xy=(.1, .8), xycoords=ax.transAxes)
+    ax.annotate("Pearsonr={:.2f}".format(r), xy=(.1, .9), xycoords=ax.transAxes)
+    # ax.annotate("Pearsonp={:.3f}".format(p), xy=(.1, .8), xycoords=ax.transAxes)
+
+
+def get_correlation_spearmanr(x, y, **kwds):
+    # https://stackoverflow.com/questions/66108908/how-to-combine-a-pairplot-and-a-triangular-heatmap
+    cmap = kwds['cmap']
+    norm = kwds['norm']
+    ax = plt.gca()
+    ax.tick_params(bottom=False, top=False, left=False, right=False)
+    sns.despine(ax=ax, bottom=True, top=True, left=True, right=True)
+    r, _ = stats.spearmanr(x, y)
+    facecolor = cmap(norm(r))
+    ax.set_facecolor(facecolor)
+    lightness = (max(facecolor[:3]) + min(facecolor[:3])) / 2
+    ax.annotate(f"Spearmanr={r:.2f}", xy=(.1, .9), xycoords=ax.transAxes,
+                color='white' if lightness < 0.7 else 'black', size=9)  # , ha='center', va='center'
 
 
 def make_joint_distribution(buildings, acceptable, discrete, plot=True):
@@ -471,11 +485,12 @@ def make_joint_distribution(buildings, acceptable, discrete, plot=True):
         # g.map_offdiag(sns.regplot)
         g.map_upper(sns.regplot)
         g.map_lower(sns.kdeplot, levels=discrete, color="k", shade=True)
+        g.map_lower(get_correlation_spearmanr, cmap=plt.get_cmap('vlag'), norm=plt.Normalize(vmin=-.5, vmax=.5))
         fig = plt.gcf()
         fig.canvas.set_window_title(f'Possible correlations among {df_tmp.shape[0]}'
                                     f' validated combination of parameters')
-        g.fig.set_size_inches(10, 7)
-        g.map(get_correlation)
+        g.fig.set_size_inches(9, 6)
+        g.map_upper(get_correlation_pearsonr)
         # g._legend.remove()
         plt.show()
         # plt.show(block=False)
@@ -561,8 +576,264 @@ def plot_combinations(var_names, bounds, combs, points, sample_nbr):
     plt.show()
 
 
+# 'eigenvectors' -> cholesky=False
+def make_random_from_correlated(data, var_names, nbr_samples, cholesky=True, plots=True):
+    """
+    This function generates random samples from a covariance matrix based on correlated samples
+    or directly from correlated samples to calculate covariance matrix first.
+    Explanation below from:
+    https://scipy-cookbook.readthedocs.io/items/CorrelatedRandomSamples.html
+
+    "To generate correlated normally distributed random samples, one can first generate uncorrelated
+    samples, and then multiply them by a matrix C such that CCT=R, where R is the desired covariance
+    matrix. C can be created, for example, by using the Cholesky decomposition of R, or from the
+    eigenvalues and eigenvectors of R."
+    """
+
+    if isinstance(data, np.ndarray):
+        values = data
+        covariance_matrix = np.cov(values, rowvar=True)  # The desired covariance matrix
+    else:
+        # from dataframe columns (joint distribution)
+        values = data.to_numpy()
+        var_names = list(data.columns.values)
+        covariance_matrix = np.cov(values, rowvar=False)  # The desired covariance matrix
+
+    # print(f'covariance_matrix:\n{covariance_matrix}')
+    # Generate samples from all independent normally distributed random variables
+    # (with mean 0 and std. dev. 1)
+    x = stats.norm.rvs(size=(len(var_names), nbr_samples))
+
+    if cholesky:
+        # Compute the Cholesky decomposition.
+        c = linalg.cholesky(covariance_matrix, lower=True)
+    else:
+        # Compute the eigenvalues and eigenvectors.
+        evals, evecs = linalg.eigh(covariance_matrix)
+        # Construct c, so c*c^T = r.
+        c = np.dot(evecs, np.diag(np.sqrt(evals)))
+
+    # Convert the data to correlated random variables
+    y = np.dot(c, x)
+
+    y_transformed = []
+    if not isinstance(data, np.ndarray):
+        # transform data to the correct range of calibrated values
+        for i, j in zip(range(len(y)), range(len(values[0]))):
+            # one-dimensional piecewise linear interpolant to a function with given discrete data points
+            # https://numpy.org/doc/stable/reference/generated/numpy.interp.html
+            y_transformed.append(np.interp(y[i], (y[i].min(), y[i].max()), (values[:, j].min(), values[:, j].max())))
+        y_transformed = np.asarray(y_transformed, dtype=np.float32)
+    else:
+        y_transformed = y
+
+    if plots:
+        combs = []
+        combinations_names = []
+        for i in range(len(var_names)):
+            for j in range(len(var_names)):
+                if i > j:
+                    combs.append((i, j))
+                    combinations_names.append((var_names[i], var_names[j]))
+
+        plots_mtx = list(range(1, (len(var_names) - 1) * (len(var_names) - 1) + 1))
+        counter = 0
+        for i in range(len(var_names) - 1):
+            for j in range(len(var_names) - 1):
+                counter += 1
+                if i < j:
+                    plots_mtx.remove(counter)
+
+        for i, j, k in zip(plots_mtx, combs, combinations_names):
+            plt.subplot(len(var_names) - 1, len(var_names) - 1, i)
+            plt.scatter(y_transformed[j[0]], y_transformed[j[1]], marker='h', c='blue')
+            plt.xlabel(k[0])
+            plt.ylabel(k[1])
+            plt.tight_layout()
+            plt.grid(False)
+
+        fig = plt.gcf()
+        fig.canvas.set_window_title(
+            "Pairwise plots of generated samples from covariance matrix of calibrated parameters")
+        plt.show()
+    return y_transformed
+
+
+def second_largest(numbers):
+    # https://stackoverflow.com/questions/16225677/get-the-second-largest-number-in-a-list-in-linear-time
+    count = 0
+    m1 = m2 = float('-inf')
+    for x in numbers:
+        count += 1
+        if x > m2:
+            if x >= m1:
+                m1, m2 = x, m1
+            else:
+                m2 = x
+    return m2 if count >= 2 else None
+
+
+def fit_best_distro(calib_params, nbr_samples, nbr_match, Danoe=True, plot=True):
+    """
+    The third parameter is used when plot option is set to 'True' for presenting the number of distributions
+    This code is inspired by Sebastian Jose's answer in stackoverflow
+    # https://stackoverflow.com/questions/6620471/fitting-empirical-distribution-to-theoretical-ones-with-scipy-python
+    """
+
+    read_time3 = time.time()
+    print(f'\t- Finding best distributions started ...')
+    distributions = ['alpha', 'anglit', 'arcsine', 'beta', 'betaprime', 'bradford', 'burr', 'cauchy', 'chi', 'chi2',
+                     'cosine', 'dgamma', 'dweibull', 'erlang', 'expon', 'exponweib', 'exponpow', 'f', 'fatiguelife',
+                     'fisk', 'foldcauchy', 'foldnorm', 'genlogistic', 'genpareto', 'genexpon', 'gumbel_r',
+                     'genextreme', 'gausshyper', 'gamma', 'gengamma', 'genhalflogistic', 'gilbrat', 'gompertz',
+                     'gumbel_l', 'halfcauchy', 'halflogistic', 'halfnorm', 'hypsecant', 'invgamma', 'invgauss',
+                     'invweibull', 'johnsonsb', 'johnsonsu', 'ksone', 'kstwobign', 'laplace', 'logistic', 'loggamma',
+                     'loglaplace', 'lognorm', 'lomax', 'maxwell', 'mielke', 'nakagami', 'ncx2', 'ncf', 'nct', 'norm',
+                     'pareto', 'pearson3', 'powerlaw', 'powerlognorm', 'powernorm', 'rdist', 'reciprocal', 'rayleigh',
+                     'rice', 'recipinvgauss', 'semicircular', 't', 'triang', 'truncexpon', 'truncnorm', 'tukeylambda',
+                     'uniform', 'vonmises', 'wald', 'weibull_min', 'weibull_max', 'wrapcauchy']
+
+    param_names = list(calib_params.columns.values)
+    # Danoe's formula to find optimum number of bins
+    if Danoe:
+        n = len(calib_params)
+        num_bins = []
+        skewness = stats.skew(calib_params, axis=0)  # skewness along column-wise
+        sigma_g1 = math.sqrt((6 * (n - 2)) / ((n + 1) * (n + 3)))
+        for skew in skewness:
+            num_bins.append(round(1 + math.log(n, 2) + math.log(1 + abs(skew) / sigma_g1, 2)))
+    else:
+        # predefined number of bins for each parameter in local_builder
+        num_bins = [nbr_samples] * len(calib_params.columns)
+
+    # Calculate Histogram
+    frequencies = []
+    bin_edges = []
+    central_values = []
+    for name, nb in zip(param_names, range(len(num_bins))):
+        f, b = np.histogram(calib_params[name], num_bins[nb], density=True)
+        frequencies.append(f)
+        bin_edges.append(b)
+        central_values.append([(bin_edges[nb][i] + bin_edges[nb][i + 1]) / 2 for i in range(len(bin_edges[nb]) - 1)])
+
+    results = {}
+    best_match = {}
+    for i, name in enumerate(param_names):
+        results[name] = {}
+        best_match[name] = {}
+        for distribution in distributions:
+            dist = getattr(stats, distribution)
+
+            fitted = dist.fit(calib_params[name])  # Get parameters of distribution
+            # Separate parts of parameters
+            arg = fitted[:-2]
+            loc = fitted[-2]
+            scale = fitted[-1]
+
+            # Calculate fitted PDF and error with fit in distribution
+            pdf_values = [dist.pdf(c, loc=loc, scale=scale, *arg) for c in central_values[i]]
+
+            # Calculate SSE (sum of squared estimate of errors)
+            sse = np.sum(np.power(frequencies[i] - pdf_values, 2.0))
+
+            # Build results and sort by sse
+            results[name][distribution] = [sse, arg, loc, scale]
+        results[name] = {k: results[name][k] for k in sorted(results[name], key=results[name].get)}
+        first_key = next(iter(results[name]))
+        best_match[name].update({first_key: next(iter(results[name].values()))})
+    print(f'\t+ Finding best distributions is done in {round((time.time() - read_time3), 2)}s!')
+
+    if plot:
+        for key in results.keys():
+            distro_to_plot = {k: results[key][k] for k in list(results[key])[:nbr_match]}
+
+            # Histogram of data
+            plt.figure(figsize=(6, 4))
+            plt.hist(calib_params[key], density=True, ec='white', color='gray')
+            plt.title(key)
+            plt.xlabel('Values')
+            plt.ylabel('Frequencies')
+
+            legends = []
+            # Plot n distributions
+            for distro, result in distro_to_plot.items():
+                legends.append(distro)
+                dist = getattr(stats, distro)
+                sse = result[0]
+                arg = result[1]
+                loc = result[2]
+                scale = result[3]
+                x_plot = np.linspace(min(calib_params[key]), max(calib_params[key]), 10)
+                y_plot = dist.pdf(x_plot, loc=loc, scale=scale, *arg)
+
+                # FIXME: A QUICK SOLUTION TO AVOID ERROR JUMP IN y_plot!
+                if second_largest(y_plot) * 10 < max(y_plot):
+                    index_max = max(range(len(y_plot)), key=y_plot.__getitem__)
+                    y_plot[index_max] = second_largest(y_plot)
+
+                plt.plot(x_plot, y_plot, label=distro + ": " + str(sse)[0:6],
+                         color=(random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1)))
+
+            plt.legend(title='Sum of squared of errors', bbox_to_anchor=(0, 1), loc='upper left')
+            fig = plt.gcf()
+            fig.canvas.set_window_title("Best theoretical distribution fits to calibrated parameters")
+            plt.show()
+
+    return best_match
+
+
+def make_random_from_continuous(distro_dict, bounds, nbr_samples, plot):
+    param_names = list(distro_dict.keys())
+    distro_names = []
+    for param in param_names:
+        distro_names += distro_dict[param].keys()
+
+    problem = {
+        'num_vars': len(param_names),
+        'names': param_names,
+        'bounds': [[0, 1]] * len(param_names)
+    }
+    salib_samples = latin.sample(problem, nbr_samples)
+    for i, dist in zip(range(len(param_names)), distro_names):
+        distro = getattr(stats, dist)
+
+        distro_values = (distro_dict[param_names[i]])[dist]
+        arg = distro_values[1]
+        loc = distro_values[2]
+        scale = distro_values[3]
+
+        # https://stackoverflow.com/questions/45280278/could-salib-support-other-probability-distribution-when-inputing-parameters-in-s
+        salib_samples[:, i] = distro.ppf(salib_samples[:, i], loc=loc, scale=scale, *arg)
+
+    # To catch out of bound values from some distributions
+    df = pd.DataFrame(salib_samples, columns=param_names)
+    for i, param_name in enumerate(param_names):
+        df[param_name] = pd.np.where(df[param_name] < bounds[i][0], bounds[i][0], df[param_name])
+        df[param_name] = pd.np.where(df[param_name] > bounds[i][1], bounds[i][1], df[param_name])
+
+    if plot:
+        sns.color_palette("rocket", as_cmap=True)
+        g = sns.PairGrid(df, palette="Paired")
+        g.map_diag(sns.histplot, kde=True)  # element="step"
+        g.map_lower(sns.kdeplot, levels=4)
+        g.map_lower(sns.scatterplot, color="gray")
+        g.map_upper(sns.regplot,
+                    scatter_kws={"color": "gray"}, line_kws={"color": "red"})
+        g.add_legend(frameon=True)
+        fig = plt.gcf()
+        fig.canvas.set_window_title(f'Random samples for parameters from calibrated ranges')
+        g.fig.set_size_inches(9, 6)
+        plt.tight_layout()
+        plt.show()
+
+    else:
+        print(df)
+
+    return salib_samples
+
+
 def calibrate_uncertain_params(params, params_ranges, nbr_sim, buildings,
-                               alpha=5, beta=85, discrete=5, plot=True):
+                               alpha=5, beta=85, discrete=5, all_plots=True):
     """
     This function is based on Carlos Cerezo, et al's method published in 2017
     This method needs:
@@ -574,11 +845,11 @@ def calibrate_uncertain_params(params, params_ranges, nbr_sim, buildings,
     """
 
     print(f'- The start of annual calibration method ...')
-    if not plot:
+    if not all_plots:
         print("** THE PLOT OPTION IS OFF, CHANGE TO 'TRUE' TO SEE THE RESULTS! **")
     # * ALGORITHM STEP1: PARAMETER DEFINITION * * * * * * * * * * /
     _, a_temp, total_epc, _, _, params_cat, params_build = read_epc_values(lb.VarName2Change, 0)
-    if plot:
+    if all_plots:
         plot_prior_distributions(params, params_ranges, buildings, discrete,
                                  per_building=False, SALib=lb.SAMPLE_TYPE, LHC=params_cat)
 
@@ -588,8 +859,8 @@ def calibrate_uncertain_params(params, params_ranges, nbr_sim, buildings,
 
     # * ALGORITHM STEP3: ERROR QUALIFICATION (α) * * * * * * * * * * /
     total_sim_results, _, errors, acceptable = \
-        compare_results(buildings, sim_data, total_epc,
-                        model_area, alpha, per_building=False, all_buildings=False)
+        compare_results(buildings, sim_data, total_epc, model_area, alpha,
+                        per_building=False, all_buildings=True, plots=all_plots)
 
     # * ALGORITHM STEP4: TEST OF ASSUMPTIONS (β) * * * * * * * * * * /
     unexplained_buildings = []
@@ -610,36 +881,36 @@ def calibrate_uncertain_params(params, params_ranges, nbr_sim, buildings,
         return 1
 
     # * ALGORITHM STEP5: DISTRIBUTION GENERATION * * * * * * * * * * /
-    calib_params = make_joint_distribution(buildings, acceptable, discrete, plot)
+    calib_params = make_joint_distribution(buildings, acceptable, discrete, plot=all_plots)
 
     # Plot below provides more insight to possible correlations of parameters /
-    if plot:
-        plot_joint_distributions(calib_params, discrete)
+    # if all_plots: # FIXME: AN EXTRA PLOT, CORRELATION OF PARAMETERS, UNCOMMENT TO SEE.
+    #    plot_joint_distributions(calib_params, discrete)
 
-    calib_frequencies = plot_calibrated_parameters(calib_params, lb.Bounds, discrete, plot)
+    calib_frequencies = plot_calibrated_parameters(calib_params, lb.Bounds, discrete, plot=all_plots)
 
     # * ALGORITHM STEP6: RANDOM SAMPLED SIMULATIONS * * * * * * * * * * /
-    # Send the result back to local_builder for simulations with θ'
-    make_final_distribution(calib_params, calib_frequencies)
+    # Save and send the result back to local_builder for simulations with θ'
+    final_samples = 100  # The number of final samples based on calibrated parameters
+    # APPROACH 1
+    theta_prime = make_random_from_correlated(calib_params, lb.VarName2Change, final_samples,
+                                               cholesky=True, plots=all_plots)
+    # np.savetxt("theta_prime.csv", theta_prime, delimiter=",") # FIXME UNCOMMENT FOR WRITE!
+
+    # APPROACH 2
+    best_distros = fit_best_distro(calib_params, lb.sample_nbr, 3, Danoe=True, plot=all_plots)
+    theta_prime = make_random_from_continuous(best_distros, lb.Bounds, final_samples, plot=all_plots)
+    # np.savetxt("theta_prime.csv", theta_prime, delimiter=",")
 
     print(f'+ Calibration method is over!')
     return 0
-
-# TODO: MAKE ARBITRARY DISTRIBUTION WORK WITH SALib
-from scipy import stats
-def make_final_distribution(calib_params, calib_frequencies):
-    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.rv_discrete.html
-    # https://stackoverflow.com/questions/45280278/could-salib-support-other-probability-distribution-when-inputing-parameters-in-s/51763834
-    for i in range(len(calib_params.columns)):
-        print(set(calib_params[calib_params.columns[i]]))
-    pass
 
 
 # CALIBRATION RUN * * * * * * * *
 if __name__ == '__main__':
     # plot_time = 5  # plots terminate in a plot_time by a multiplayer!
     calibrate_uncertain_params(lb.VarName2Change, lb.Bounds, lb.NbRuns, lb.BuildNum,
-                               alpha=20, beta=90, discrete=lb.sample_nbr, plot=True)
+                               alpha=7, beta=90, discrete=lb.sample_nbr, all_plots=True)
 
     # Prior presentation of real samples for selected parameters
     if lb.SAMPLE_TYPE:
@@ -648,5 +919,3 @@ if __name__ == '__main__':
 
     print(f"* Execution time:{round((time.time() - start_time), 2)}s /"
           f" {round(((time.time() - start_time) / 60), 2)}min!")
-
-
