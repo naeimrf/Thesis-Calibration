3c4735b2f87116e73813dee887cb25d588a49b73
naeim <naeimrf@gmail.com>
2021-04-23 11:14:44 +0200

    V1.7 sensitivity explanation in local_setup

--- local_setup.py
+++ local_setup.py
@@ -6,6 +6,9 @@ This script is a central place to hold initial parameters for all local scripts!
 path2addgeom = os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), 'geomeppy')
 sys.path.append(path2addgeom)
 
+# RUNNING SENSITIVITY ANALYSIS IS COMPLETELY INDEPENDENT OF BOOLEANS IN OPTION 1 AND 2, ONLY SET:
+#   -> CaseName, BuildNum, VarName2Change, Bounds AND NbRuns BASED ON YOUR ALREADY SIMULATED FILES!
+
 # OPTION 1: ALWAYS KEEP RECURSIVE_CALIBRATION = False <-
 # 1. FIRST RUN
 #   -> SIMULATE_TO_CALIBRATE = True AND CALIBRATE_WITH_CALIBRATED_PARAMETERS = False THEN RUN local_builder.py
@@ -16,24 +19,24 @@ sys.path.append(path2addgeom)
 # 3. TO SIMULATE WITH CALIBRATED PARAMETERS
 #   -> SIMULATE_TO_CALIBRATE = True AND CALIBRATE_WITH_CALIBRATED_PARAMETERS = True THEN RUN local_builder.py
 
-SIMULATE_TO_CALIBRATE = False
+SIMULATE_TO_CALIBRATE = True
 CALIBRATE_WITH_CALIBRATED_PARAMETERS = False
 
 # OPTION 2: SET THE BOOLEAN BELOW TO 'TRUE' TO RUN 'local_builder' RECURSIVELY,
 # UNTIL ALL BUILDINGS CAN SATISFY BETA VALUE IN CALIBRATION ALGORITHM!
 # THIS OPTION IS SUITABLE FOR SEVERAL-TIME CALIBRATIONS BUT WITH LOW NUMBER OF SIMULATIONS!
-RECURSIVE_CALIBRATION = True
+RECURSIVE_CALIBRATION = False
 
 
-CaseName = 'ForTest_6_20'  # a folder_name to save input and outfile in form of pickles!
-BuildNum = [6]  # building numbers (names) to be simulated  #  [i for i in range(7, 28)]
+CaseName = 'ForTest_9_10_126st'  # a folder_name to save input and outfile in form of pickles!
+BuildNum = [9, 10]  # building numbers (names) to be simulated  #  [i for i in range(7, 28)]
 # Before each run, pay attention to Outputs.txt (lines: 110, 353, 359)
 VarName2Change = ['EnvLeak', 'wwr', 'IntLoadMultiplier', 'AreaBasedFlowRate', 'setTempLoL']
 Bounds = [[0.4, 2], [0.2, 0.4], [0.5, 2], [0.3, 0.6], [19, 22]]
 
 # Morris analysis needs 'NbRuns' to be a multiplier of 'number of parameters'
 # That is if you have 5 parameters you need (5+1) * k number simulations. Ex: 17x6=102
-NbRuns = 20
+NbRuns = 126
 
 # ** EXPERIMENTAL CODE **
 # SALib or SKOPT LHC-Centered
